ad0ecf580155320ca6b7075a16f3ef420380c0e1
Cristian Ciortea <cristian.ciortea@syneto.eu>
2024-06-29 20:36:41 +0300

    Add json types for request and reponse. Add some tests for the validations.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use axum::extract::{Path, Query};
+use axum::extract::Json;
 use axum::http::StatusCode;
-use axum::response::{Html, IntoResponse};
-use axum::routing::get;
+use axum::response::{IntoResponse, Response, Json as JsonResponse};
 use axum::Router;
-use serde::Deserialize;
+use axum::routing::{get, post};
+use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
 
 static SERVER_ADDRESS: &str = "0.0.0.0:8000";
@@ -11,7 +11,7 @@ static SERVER_ADDRESS: &str = "0.0.0.0:8000";
 #[tokio::main]
 async fn main() -> Result<(), ()> {
     let health_router = Router::new().route("/health", get(health));
-    let calculate_router = Router::new().route("/calculate", get(calculate));
+    let calculate_router = Router::new().route("/calculate", post(calculate));
 
     let main_router = Router::new().merge(health_router).merge(calculate_router);
 
@@ -28,12 +28,81 @@ async fn health() -> impl IntoResponse {
     (StatusCode::OK, "The server is working.")
 }
 
-#[derive(Debug, Deserialize)]
-struct CalculateParams {
-    name: Option<String>,
+#[derive(Debug)]
+enum Currency {
+    DOLLAR,
+    RON,
+    EURO,
 }
-async fn calculate(Query(params): Query<CalculateParams>) -> impl IntoResponse {
-    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
-    let name = params.name.as_deref().unwrap_or("");
-    Html(format!("Hello <strong>{name}</strong>"))
+
+impl Currency {
+    fn from_str(currency: &str) -> Option<Currency> {
+        match currency.trim().to_uppercase().as_str() {
+            "DOLLAR" => Some(Currency::DOLLAR),
+            "RON" => Some(Currency::RON),
+            "EURO" => Some(Currency::EURO),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct CalculateParams {
+    salary: Option<String>,
+    currency: Option<String>,
+    custom_tax: Option<String>,
+}
+
+#[derive(Debug, Serialize)]
+struct CalculateResults {
+    brute_salary: u32,
+    net_salary: u32,
+    cass: u32,
+    cas: u32,
+    income_tax: u32,
+    personal_deduction: u32,
+}
+
+#[derive(Debug, Serialize)]
+struct ErrorResponse {
+    message: String,
+}
+
+async fn calculate(Json(data): Json<CalculateParams>) -> Response {
+    println!("->> {:<12} - Calculate handler - {data:?}", "HANDLER");
+    let salary: u32 = match data.salary.as_deref().unwrap_or("").trim().parse() {
+        Ok(sal) => sal,
+        Err(_) => {
+            let error_response = ErrorResponse {
+                message: "Invalid or missing salary".to_string(),
+            };
+            return (StatusCode::NOT_ACCEPTABLE, error_response.message).into_response();
+        }
+    };
+    let currency = match data.currency.as_deref().and_then(|c| Currency::from_str(c)) {
+        Some(currency) => currency,
+        None => {
+            let error_response = ErrorResponse {
+                message: "Currency not supported.".to_string(),
+            };
+            return (StatusCode::NOT_ACCEPTABLE, error_response.message).into_response();
+        }
+    };
+    let custom_tax: Option<u32> = data.custom_tax.as_deref()
+        .get_or_insert("")
+        .trim()
+        .parse()
+        .ok();
+
+    let calculate_results = CalculateResults {
+        brute_salary: salary,
+        net_salary: 0,
+        cass: 0,
+        cas: 0,
+        income_tax: 0,
+        personal_deduction: 0,
+    };
+
+    Json(calculate_results).into_response()
 }
--- tests/test_calculate.rs
+++ tests/test_calculate.rs
@@ -1,5 +1,8 @@
 use anyhow::Result;
 use axum::http::StatusCode;
+use serde_json::{json};
+
+static LOCALHOST: &str = "http://localhost:8000";
 
 #[tokio::test]
 async fn check_health() -> Result<()> {
@@ -14,10 +17,63 @@ async fn check_health() -> Result<()> {
     Ok(())
 }
 
+
 #[tokio::test]
-async fn calculate_salary() -> Result<()> {
-    let client = httpc_test::new_client("http://localhost:8000")?;
-    let response = client.do_get("/calculate?name=Axum").await?;
-    response.print().await?;
+async fn calculate_with_invalid_currency_should_return_error_400() {
+    let client = reqwest::Client::new();
+    let data = json!({
+        "salary": "1000",
+        "currency": "YEN", // Invalid currency.
+        "customTax": "500",
+    });
+
+    let response = client.post(format!("{LOCALHOST}/calculate"))
+        .json(&data)
+        .send()
+        .await
+        .expect("Failed to send request.");
+
+    assert_eq!(response.status(), StatusCode::NOT_ACCEPTABLE.as_u16());
+    let text = response.text().await.expect("Failed to read response body");
+    assert!(text.contains("Currency not supported"));
+}
+
+#[tokio::test]
+async fn calculate_missing_salary() {
+    let client = reqwest::Client::new();
+    let data = json!({
+        // Missing "salary"
+        "currency": "RON",
+        "customTax": "500",
+    });
+
+    let response = client.post(format!("{LOCALHOST}/calculate"))
+        .json(&data)
+        .send()
+        .await
+        .expect("Failed to send request");
+
+    assert_eq!(response.status(), StatusCode::NOT_ACCEPTABLE.as_u16());
+    let text = response.text().await.unwrap();
+    assert!(text.contains("Invalid or missing salary"));
+}
+
+#[tokio::test]
+async fn calculate_salary_happy_path() -> Result<()> {
+    let client = reqwest::Client::new();
+    let data = json!({
+        "salary": "1000",
+        "currency": "dollar",
+        "customTax": "abc",
+    });
+    let response = client.post(format!("{LOCALHOST}/calculate"))
+        .json(&data)
+        .send()
+        .await?;
+    let status = response.status();
+    let text = response.text().await.unwrap();
+    println!("{:?}", text);
+    assert_eq!(status, StatusCode::OK.as_u16());
+
     Ok(())
 }
